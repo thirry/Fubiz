3455d1113e03b51daa00edf383dc3b7530e0aa61
Thrirry <tam300497@gmail.com>
2018-03-01 15:34:42 +0700

    update afternoon - 1/march

--- app/src/main/java/com/ev/gone/fubiz/Views/AlphaActivity.java
+++ app/src/main/java/com/ev/gone/fubiz/Views/AlphaActivity.java
@@ -1,4 +1,4 @@
-package com.ev.gone.fubiz;
+package com.ev.gone.fubiz.Views;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -6,15 +6,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.ev.gone.fubiz.R;
+
 /**
  * Created by gone on 2/24/18.
  */
 
-public class AlphaActivity extends AppCompatActivity {
+public class AlphaActivity extends AppCompatActivity{
 
     Button btn_backtohome;
     Button setting;
     Button time_setting;
+
+    Button alpha_ic;
+    boolean isPlay = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +66,59 @@ public class AlphaActivity extends AppCompatActivity {
 
         });
 
+        //animate fo button
+        alpha_ic = (Button) findViewById(R.id.alpha_ic);
+        alpha_ic.setOnClickListener(mTogglePlayButton);
+
+//        alpha_ic.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                if (v == alpha_ic){
+//                    alpha_ic.setBackgroundResource(R.mipmap.alphaic_active);
+//                }
+//            }
+//
+//        });
+
+
 
     }
+
+    View.OnClickListener mTogglePlayButton = new View.OnClickListener(){
+
+        @Override
+        public void onClick(View v){
+            // change your button background
+
+            if(isPlay){
+                v.setBackgroundResource(R.mipmap.alphaic);
+            }else{
+                v.setBackgroundResource(R.drawable.alphaic_active);
+            }
+
+            isPlay = !isPlay; // reverse
+        }
+
+    };
+
+//    @Override
+//    public void onClick(View v) {
+//
+//        alpha_ic = (Button) findViewById(R.id.alpha_ic);
+//
+//        if (v == alpha_ic){
+//
+//            alpha_ic.setBackgroundResource(R.mipmap.alphaic_active);
+//
+//        }else{
+//
+//            alpha_ic.setBackgroundResource(R.mipmap.alphaic);
+//
+//        }
+//
+//    }
+
+
+
 }
