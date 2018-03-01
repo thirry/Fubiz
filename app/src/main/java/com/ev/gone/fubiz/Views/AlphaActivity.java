c3715c3c30551bbc9875b546a772253aa04d9aba
Thrirry <tam300497@gmail.com>
2018-03-01 18:52:26 +0700

    update afternoon - 1/march - last

--- app/src/main/java/com/ev/gone/fubiz/Views/AlphaActivity.java
+++ app/src/main/java/com/ev/gone/fubiz/Views/AlphaActivity.java
@@ -12,14 +12,20 @@ import com.ev.gone.fubiz.R;
  * Created by gone on 2/24/18.
  */
 
-public class AlphaActivity extends AppCompatActivity{
+public class AlphaActivity extends AppCompatActivity {
 
     Button btn_backtohome;
     Button setting;
     Button time_setting;
 
     Button alpha_ic;
+    Button volume_ocean;
     boolean isPlay = false;
+
+    boolean isOcean = false;
+    boolean isOcean_nd = true;
+    boolean isOcean_rd = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,7 +74,9 @@ public class AlphaActivity extends AppCompatActivity{
 
         //animate fo button
         alpha_ic = (Button) findViewById(R.id.alpha_ic);
-        alpha_ic.setOnClickListener(mTogglePlayButton);
+        alpha_ic.setOnClickListener(alphaTogglePlayButton);
+        volume_ocean = (Button) findViewById(R.id.ocean_volume);
+        volume_ocean.setOnClickListener(oceanTogglePlayButton);
 
 //        alpha_ic.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -82,25 +90,95 @@ public class AlphaActivity extends AppCompatActivity{
 //        });
 
 
-
     }
 
-    View.OnClickListener mTogglePlayButton = new View.OnClickListener(){
+    View.OnClickListener alphaTogglePlayButton = new View.OnClickListener() {
 
         @Override
-        public void onClick(View v){
-            // change your button background
+        public void onClick(View v) {
 
-            if(isPlay){
+            if (isPlay) {
                 v.setBackgroundResource(R.mipmap.alphaic);
-            }else{
-                v.setBackgroundResource(R.drawable.alphaic_active);
+            } else {
+                v.setBackgroundResource(R.mipmap.alphaic_active);
             }
 
             isPlay = !isPlay; // reverse
         }
 
     };
+
+
+    View.OnClickListener oceanTogglePlayButton = new View.OnClickListener() {
+
+        View a;
+
+        @Override
+        public void onClick(View v) {
+
+
+            if (isOcean && isOcean_nd && isOcean_rd) {
+                v.setBackgroundResource(R.mipmap.volume_ocean_max);
+
+
+            } else if (!isOcean && isOcean_nd && isOcean_rd) {
+                v.setBackgroundResource(R.mipmap.volume_ocean_low);
+                isOcean_nd = !isOcean_nd;
+
+            } else if (isOcean && !isOcean_nd && isOcean_rd) {
+                v.setBackgroundResource(R.mipmap.volume_ocean_middle);
+//                isOcean_nd = isOcean_nd;
+                isOcean = !isOcean;
+                isOcean_rd = !isOcean_rd;
+
+            } else if (isOcean && !isOcean_nd && !isOcean_rd) {
+                v.setBackgroundResource(R.mipmap.volume_ocean_max);
+                isOcean_nd = !isOcean_nd;
+//                isOcean_rd =!isOcean_rd;
+            } else if (isOcean && isOcean_nd && !isOcean_rd) {
+                v.setBackgroundResource(R.mipmap.volume_ocean_mute);
+                isOcean_rd = !isOcean_rd;
+
+            } else if (isOcean && isOcean_nd && isOcean_rd) {
+                v.setBackgroundResource(R.mipmap.volume_ocean_low);
+            }
+
+
+            isOcean = !isOcean;
+
+//            if (isOcean) {
+//                v.setBackgroundResource(R.mipmap.volume_ocean_max);
+//                isOcean_nd = false;
+//
+//            } else if (isOcean_nd = false) {
+//                v.setBackgroundResource(R.mipmap.volume_ocean_low);
+//                isOcean_nd = true;
+//                isOcean = true;
+//
+//            } else if(isOcean = true) {
+//                v.setBackgroundResource(R.mipmap.volume_ocean_middle);
+//                isOcean = false;
+//            }
+
+//            if (isOcean_nd = true){
+//                v.setBackgroundResource(R.mipmap.volume_ocean_middle);
+//
+//            }else{
+//                v.setBackgroundResource(R.mipmap.volume_ocean_max);
+//            }
+
+//            if (!isOcean){
+//                v.setBackgroundResource(R.mipmap.volume_ocean_middle);
+//            }else{
+//                v.setBackgroundResource(R.mipmap.volume_ocean_max);
+//
+//            }
+
+//            isOcean = !isOcean; // reverse
+        }
+
+    };
+
 
 //    @Override
 //    public void onClick(View v) {
@@ -118,7 +196,6 @@ public class AlphaActivity extends AppCompatActivity{
 //        }
 //
 //    }
-
 
 
 }
